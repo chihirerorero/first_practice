fcc89fb2a256797a4b7dc8e46aa4eeaf46bd40fd
chihirerorero <108926960+chihirerorero@users.noreply.github.com>
2022-07-08 18:28:58 +0900

    Add files via upload

---
+++ 単語カード/word.js
@@ -0,0 +1,390 @@
+let ja = document.getElementById('ja');
+let en = document.getElementById('en');
+
+let min = 0 ;
+let max = 9 ;
+
+let r = 0
+let m = -1
+
+let a = Math.floor( Math.random() * (max + 1 - min) ) + min;
+
+document.getElementById('answer_btn').addEventListener('click' ,()=>{
+     document.getElementById('en').style.color = '#FFF';
+})
+
+
+let word = ()=>{
+    document.getElementById('en').style.color = '#191919';
+            a = Math.floor( Math.random() * (max + 1 - min) ) + min ; 
+if(a == 0){
+    ja.innerHTML = '何かをするだけの余裕を持たせる';
+    en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 4){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 5){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 6){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 6){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 7){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 8){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 9){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 10){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+    ja.innerHTML = '';
+    en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 1){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 2){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+if(a == ){
+   if(a == ){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+    ja.innerHTML = '';
+    en.innerHTML = '';
+}
+if(a == 0){
+    ja.innerHTML = '';
+    en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+if(a == 0){
+   ja.innerHTML = '';
+   en.innerHTML = '';
+}
+
+}
+document.addEventListener('keydown',(e)=>{
+
+    console.log(e.key);
+    if (e.key == 'Enter') {word()}
+})
+
+
+let N = 0;
+document.addEventListener('keydown',(e)=>{
+
+     if(e.key == en.innerHTML[N]){
+
+         N = N+1;
+         console.log(`N=${N}en=${en.innerHTML.length}`)
+         document.querySelector( 'input' ).value += e.key;
+            if(en.innerHTML.length == N){
+            N = 0;
+             word();
+             document.getElementById('put_key').value = '';
+            }
+        r = r+1;
+        document.getElementById('r_word').innerHTML = '正解文字数:'+r;
+     }
+     else{
+         document.getElementById('put_key').value = '';
+         N = 0;
+         m = m+1;
+         document.getElementById('m_word').innerHTML = '不正解文字数:' +m;
+     }
+})
